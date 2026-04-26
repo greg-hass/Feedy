4db173051620604dd3dee3d821b1cc1c3a06559c
Greg <galcorn@icloud.com>
2026-04-26 19:21:57 +0100

    Widen YouTube thumbnail fallbacks

--- src/lib/feed/youtube.ts
+++ src/lib/feed/youtube.ts
@@ -73,8 +73,11 @@ const shortsProbeCache = new Map<string, Promise<boolean>>();
 export function getYouTubeThumbnailUrls(videoId: string) {
   return [
     `https://i.ytimg.com/vi/${videoId}/maxresdefault.jpg`,
+    `https://i.ytimg.com/vi/${videoId}/mqdefault.jpg`,
     `https://i.ytimg.com/vi/${videoId}/hqdefault.jpg`,
     `https://i.ytimg.com/vi/${videoId}/sddefault.jpg`,
+    `https://i.ytimg.com/vi/${videoId}/default.jpg`,
+    `https://img.youtube.com/vi/${videoId}/0.jpg`,
   ];
 }
 
